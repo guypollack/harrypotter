ae44b92d4e66b53ed05d2cebe90468e107b4425e
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-10-06 10:02:09 +0300

    Render all KeyboardRows

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -42,6 +42,11 @@ export function Keyboard(props: {
   setIncorrectGuesses: React.Dispatch<React.SetStateAction<string[]>>;
 }) {
   const keys = ["A", "B", "C"];
+  const keyboardRowLetters = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Z", "X", "C", "V", "B", "N", "M"],
+  ];
 
   return (
     // <div className="flex">
@@ -77,15 +82,30 @@ export function Keyboard(props: {
     //     />
     //   ))}
     // </div>
-    <KeyboardRow
-      keys={["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]}
-      remainingLetters={props.remainingLetters}
-      setRemainingLetters={props.setRemainingLetters}
-      correctGuesses={props.correctGuesses}
-      setCorrectGuesses={props.setCorrectGuesses}
-      incorrectGuesses={props.incorrectGuesses}
-      setIncorrectGuesses={props.setIncorrectGuesses}
-      handleGuess={handleGuess}
-    />
+    // <KeyboardRow
+    // keys={["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]}
+    // remainingLetters={props.remainingLetters}
+    // setRemainingLetters={props.setRemainingLetters}
+    // correctGuesses={props.correctGuesses}
+    // setCorrectGuesses={props.setCorrectGuesses}
+    // incorrectGuesses={props.incorrectGuesses}
+    // setIncorrectGuesses={props.setIncorrectGuesses}
+    // handleGuess={handleGuess}
+    // />
+    <div>
+      {keyboardRowLetters.map((row, index) => (
+        <KeyboardRow
+          keys={row}
+          remainingLetters={props.remainingLetters}
+          setRemainingLetters={props.setRemainingLetters}
+          correctGuesses={props.correctGuesses}
+          setCorrectGuesses={props.setCorrectGuesses}
+          incorrectGuesses={props.incorrectGuesses}
+          setIncorrectGuesses={props.setIncorrectGuesses}
+          handleGuess={handleGuess}
+          key={`KeyboardRow-${index}`}
+        />
+      ))}
+    </div>
   );
 }
