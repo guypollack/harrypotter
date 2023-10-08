02fa7e5a4c086d3bc587beaa5b50ef1360602f67
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-10-08 21:26:26 +0300

    Update import

--- src/components/SecretWord.tsx
+++ src/components/SecretWord.tsx
@@ -1,4 +1,4 @@
-import { generateWord } from "../lib/helpers/generateWord";
+import { generateWord } from "../lib/generateWord";
 
 export function SecretWord() {
   return <h1 className="text-3xl font-bold underline">{generateWord()}</h1>;
