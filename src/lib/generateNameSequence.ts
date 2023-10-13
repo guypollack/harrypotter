ebe44a7d8f4b92877a6153d0ec9b17eeb8211a35
Guy Pollack <guypollack@Guys-MacBook-Pro.local>
2023-10-13 16:28:28 +0100

    Generate sequence of character and spell names

--- src/lib/generateNameSequence.ts
+++ src/lib/generateNameSequence.ts
@@ -1,3 +1,15 @@
+import { allNames } from "../data/allNames";
+import { allSpells } from "../data/allSpells";
+
 export function generateNameSequence(names: string[]) {
   return names.sort(() => 0.5 - Math.random());
+}
+
+export function generateNameSequence2(): [string, string][] {
+  const namesArray = allNames.map((name) => ["Character", name]);
+  const spellsArray = allSpells.map((spell) => ["Spell", spell]);
+  return [...namesArray, ...spellsArray].sort(() => Math.random() - 0.5) as [
+    string,
+    string
+  ][];
 }
